5e7d3001103d57280db13d219ff74b0621665e74
wj <807388755@qq.com>
2017-09-22 14:51:39 +0800

    update at 2017/09/22

--- app/src/main/java/com/example/swapanytime/MainActivity.java
+++ app/src/main/java/com/example/swapanytime/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.swapanytime;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
