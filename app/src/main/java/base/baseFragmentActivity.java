389f7ef8fd065e0c3bd97ef1093ef050a038316e
wj <18814129361@163.com>
2017-09-26 17:34:17 +0800

    新增baseFragmentActivity

---
+++ app/src/main/java/base/baseFragmentActivity.java
@@ -0,0 +1,15 @@
+package base;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by weijie on 2017/9/26.
+ */
+
+public class baseFragmentActivity extends FragmentActivity{
+
+
+}
